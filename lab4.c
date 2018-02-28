3e03b84630b803558c506be418f8161db95cd49a
Stephen <steve.ward2@gmail.com>
2018-02-28 01:50:20 -0500

    updated with another for loop

--- lab4.c
+++ lab4.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAX 1000;
+
 int main(){
 
     //three arrays
-    int one[1000];
-    int two[1000];
-    int three[1000];
+    int one[MAX];
+    int two[MAX];
+    int three[MAX];
     int sum = 0;
 
     srand(time(NULL));
-    for (int i = 0; i <= 1000; i++){
+    for (int i = 0; i <= MAX; i++){
       one[i] =  rand() % 1000;
       two[i] =  rand() % 1000;
+    }
+    //
+    for (int i = 0; i <= MAX; i++){
       three[i] = one[i] + two[i];
       sum += three[i];
     }
